b26bdf1ffdcaaeeec2e72b865835b0ca977a7c61
MRL1998 <71321675+MRL1998@users.noreply.github.com>
2022-12-08 03:15:34 +0100

    Update add_logo.py

--- personal-website-streamlit/utils/add_logo.py
+++ personal-website-streamlit/utils/add_logo.py
@@ -2,6 +2,7 @@ import streamlit as st
 from PIL import Image
 import base64
 import streamlit as st
+import base64
 
 """
     add_logo(): 
@@ -10,6 +11,8 @@ import streamlit as st
         Output: Returns markdown with Logo and Company Name above pages
 
 """
+
+
 
 def add_logo():
     st.markdown(
@@ -33,3 +36,49 @@ def add_logo():
         """,
         unsafe_allow_html=True,
     )
+
+import base64
+import streamlit as st
+
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(png_file):
+    with open(png_file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+def build_markup_for_logo(
+    png_file,
+    background_position="50% 10%",
+    margin_top="0%",
+    image_width="60%",
+    image_height="",
+):
+    binary_string = get_base64_of_bin_file(png_file)
+    return """
+            <style>
+                [data-testid="stSidebarNav"] {
+                    background-image: url("data:image/png;base64,%s");
+                    background-repeat: no-repeat;
+                    background-position: %s;
+                    margin-top: %s;
+                    background-size: %s %s;
+                }
+            </style>
+            """ % (
+        binary_string,
+        background_position,
+        margin_top,
+        image_width,
+        image_height,
+    )
+
+
+def add_logo2(png_file):
+    logo_markup = build_markup_for_logo(png_file)
+    st.markdown(
+        logo_markup,
+        unsafe_allow_html=True,
+    )
+
